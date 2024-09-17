20e4ddcb2ae5837640355cb28f2cc74cfc4ceefe
joseantoniojuniod <jose_antonio_alves@protonmail.com>
2024-09-17 12:33:22 -0300

    Aula1709 - autenticação

---
+++ nomadelife-front/src/hooks/useAuthentication.jsx
@@ -0,0 +1,22 @@
+import {
+    getAuth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    updateProfile,
+    signOut
+} from 'firebase/auth'
+import { useState, useEffect } from 'react'
+
+export const useAuthentication = () =>{
+    const [error, setError] = useState(null)
+    const [loading, serloading] = useState(null)
+    const [cancelled, setCancelled] = useState(false)
+
+    const auth = getAuth()
+
+    function checkIfIsCancelled(){
+        if(cancelled){
+            return
+        }
+    }
+}
